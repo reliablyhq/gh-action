708633a08963362c41c099641a756deea6a9a4f9
David Martin <david@chaosiq.io>
2020-10-26 17:18:47 +0100

    Try opa eval command
    Signed-off-by: David Martin <david@chaosiq.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,5 @@
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
 sh -c "echo $*"
+
+opa eval manifest.yaml -d ${INPUT_POLICIES} --format pretty
