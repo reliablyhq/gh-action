eb6db5163c158d3e23cda22132901fe70e50d42e
David Martin <david@chaosiq.io>
2020-10-27 16:26:53 +0100

    Debug
    Signed-off-by: David Martin <david@chaosiq.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,20 +17,20 @@ do
   # split manifest into multiples files, in case it contain several resources
   csplit --quiet --prefix="$MANIFESTS/manifest" $file "/---/" "{*}"
 
-  echo "list manifests subfolder"
+  echo "list manifests subfolder '$MANIFESTS'"
   ls $MANIFESTS
+
+  echo "list policies subfolder '${INPUT_POLICIES}'"
+  ls ${INPUT_POLICIES}
 
   # iterate over the split files
   for manifest in $MANIFESTS/*
   do
 
     echo "Validate manifest $manifest"
-    cat $manifest
 
     # convert the manifest from yaml to json (opa only accepts json)
     yaml2json $manifest > $manifest.json
-
-    cat $manifest.json
 
     # run the policies/rules validation - NON-breaking call
     opa eval -i $manifest.json -d ${INPUT_POLICIES} --format pretty '"data"'
