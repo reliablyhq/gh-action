e4ea5303783649da4fb552ccce61cec4832e4f39
David Martin <david@chaosiq.io>
2020-10-27 14:23:27 +0100

    Do not break script if opa eval command fails
    Signed-off-by: David Martin <david@chaosiq.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ sh -c "echo $*"
 for file in ${INPUT_FILES}
 do
   echo "Process manifest $file"
-  opa eval $file -d ${INPUT_POLICIES} --format pretty "data"
+  opa eval $file -d ${INPUT_POLICIES} --format pretty "data" || true
 done
