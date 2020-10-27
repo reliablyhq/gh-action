651f23607f7f38f822c6fe9b2dd1ca6d9b02f1cb
David Martin <david@chaosiq.io>
2020-10-27 15:46:48 +0100

    convert in-place yaml to json
    Signed-off-by: David Martin <david@chaosiq.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,10 +22,11 @@ do
   do
 
     # convert the manifest from yaml to json (opa only accepts json)
-    yaml2json $manifest > $manifest.json
+    # modify the file in-place
+    yaml2json $manifest > $manifest
 
     # run the policies/rules validation - NON-breaking call
-    opa eval $manifest.json -d ${INPUT_POLICIES} --format pretty '"data"' || true
+    opa eval $manifest -d ${INPUT_POLICIES} --format pretty '"data"' || true
 
   done
 
