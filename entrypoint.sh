8534c8a612e62d4208f542dfc478713707fecb62
David Martin <david@chaosiq.io>
2020-10-28 16:35:14 +0100

    fix echo filename to report
    Signed-off-by: David Martin <david@chaosiq.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ violationCount=0
 for file in ${INPUT_FILES}
 do
   #echo "Process manifest '$file'"
-  echo "$file" >> $REPORT
+  echo "'$file'" >> $REPORT
   echo "----------" >> $REPORT
 
   # split manifest into multiples files, in case it contain several resources
